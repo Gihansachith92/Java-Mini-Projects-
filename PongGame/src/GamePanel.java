3cce3e32cff13e90d3f8c8ffffc5fab548ab0ce5
Gihan_Sachith <gihansachith92@gmail.com>
2024-05-22 00:43:13 +0530

    coding

--- PongGame/src/GamePanel.java
+++ PongGame/src/GamePanel.java
@@ -6,7 +6,7 @@ import javax.swing.*;
 public class GamePanel extends JPanel implements Runnable{
 
     static final int GAME_WIDTH = 1000;
-    static final int GAME_HEIGHT = (int)(GAME_WIDTH * (5/9));
+    static final int GAME_HEIGHT = (int)(GAME_WIDTH * (0.5555));
     static final Dimension SCREEN_SIZE = new Dimension(GAME_WIDTH,GAME_HEIGHT);
     static final int BALL_DIAMETER = 20;
     static final int PADDLE_WIDTH = 25;
@@ -22,7 +22,15 @@ public class GamePanel extends JPanel implements Runnable{
 
 
     GamePanel(){
+       newPaddles();
+       newBall();
+        score = new Score(GAME_WIDTH,GAME_HEIGHT);
+        this.setFocusable(true);
+        this.addKeyListener(new AL());
+        this.setPreferredSize(SCREEN_SIZE);
 
+        gameThread = new Thread(this);
+        gameThread.start();
     }
 
     public void newBall(){
@@ -30,15 +38,20 @@ public class GamePanel extends JPanel implements Runnable{
     }
 
     public void newPaddles(){
-
+         paddle1 = new Paddle(0, (GAME_HEIGHT/2)-(PADDLE_HEIGHT/2),PADDLE_WIDTH,PADDLE_HEIGHT,1);
+         paddle2 = new Paddle(GAME_WIDTH-PADDLE_WIDTH, (GAME_HEIGHT/2)-(PADDLE_HEIGHT/2),PADDLE_WIDTH,PADDLE_HEIGHT,2);
     }
 
     public void paint(Graphics g){
-
+           image = createImage(getWidth(),getHeight());
+           graphics = image.getGraphics();
+           draw(graphics);
+           g.drawImage(image,0,0,this);
     }
 
     public void draw(Graphics g){
-
+         paddle1.draw(g);
+         paddle2.draw(g);
     }
 
     public void move(){
@@ -50,17 +63,36 @@ public class GamePanel extends JPanel implements Runnable{
     }
 
     public void run(){
+       //game loop
+        long lastTime = System.nanoTime();
+        double amountOfTicks = 60.0;
+        double ns = 1000000000 / amountOfTicks;
+        double delta = 0;
+        while(true){
+            long now = System.nanoTime();
+            delta += (now - lastTime) / ns;
+            lastTime = now;
+            if (delta >= 1){
+                move();
+                checkCollision();
+                repaint();
+                delta--;
+            }
+
+        }
 
     }
 
 
     public class AL extends KeyAdapter{
         public void KeyPressed(KeyEvent e){
-
+            paddle1.KeyPressed(e);
+            paddle2.KeyPressed(e);
         }
 
         public void KeyReleased(KeyEvent e){
-
+            paddle1.KeyReleased(e);
+            paddle2.KeyReleased(e);
         }
 
 
--- PongGame/src/Score.java
+++ PongGame/src/Score.java
@@ -10,7 +10,7 @@ public class Score extends Rectangle{
     int player1;
     int player2;
 
-    Score(){
+    Score(int GAME_WIDTH, int GAME_HEIGHT){
 
     }
 
