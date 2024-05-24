d319f4438eef45d19fc7e7c69e1089ca5753a473
Gihan_Sachith <gihansachith92@gmail.com>
2024-05-25 00:09:38 +0530

    coding

---
+++ SnakeGame/src/GamePanel.java
@@ -0,0 +1,47 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.Random;
+
+
+public class GamePanel extends JPanel implements ActionListener {
+
+    GamePanel(){
+
+    }
+
+    public void startGame(){
+
+    }
+    public void paintComponent(Graphics g){
+
+    }
+    public void draw(Graphics g){
+
+    }
+    public void move(){
+
+    }
+
+    public void checkApple(){
+
+    }
+    public void checkCollisions(){
+
+    }
+    public void gameOver(Graphics g){
+
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    public  class MyKeyAdapter extends KeyAdapter  {
+        public void KeyPressed(KeyEvent e){
+
+        }
+
+
+    }
+}
--- SnakeGame/src/SnakeGame.java
+++ SnakeGame/src/SnakeGame.java
@@ -2,5 +2,7 @@ public class SnakeGame {
 
     public static void main(String[] args){
 
+         new GameFrame();
+
     }
 }
